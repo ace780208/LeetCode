56fa63c7dadfc5570c7d1caf710afdaaa9154559
Hsiao-Chien Shih <jxd270700@hotmail.com>
2025-05-03 09:31:17 -0700

    Time: 3 ms (95.57%), Space: 18 MB (55.62%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,12 +1,14 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         pair = {}
+        seen = set()
         for index in range(len(s)):
             if s[index] in pair:
                 if pair[s[index]] != t[index]:
                     return False
             else:
+                if t[index] in seen:
+                    return False
                 pair[s[index]] = t[index]
-
+                seen.add(t[index])
         return True
-        
