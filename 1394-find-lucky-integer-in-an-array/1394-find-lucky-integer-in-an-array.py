80abd3b1deb78f2e9cf5d7166ba91c0685f3eb9b
Hsiao-Chien Shih <jxd270700@hotmail.com>
2025-05-13 16:28:35 -0700

    Time: 0 ms (100%), Space: 17.9 MB (49.91%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.py
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.py
@@ -2,12 +2,16 @@ class Solution:
     def findLucky(self, arr: List[int]) -> int:
         # create a hashmap to store the frequency of a value in the arr
         counts = {}
+        ans = -1
         for val in arr:
             counts[val] = counts.get(val, 0) + 1
+            if counts[val] == val:
+                ans = max(ans, val)
 
         # loop the hashmap to find the maximum lucky integer
         ans = -1
         for key, freq in counts.items():
             if key == freq:
                 ans = max(ans, key)
+
         return ans
