0485d6b026a676b463d4d1f39aeb4d2e497774d3
Hsiao-Chien Shih <jxd270700@hotmail.com>
2025-05-14 16:21:07 -0700

    Time: 0 ms (100%), Space: 17.7 MB (90.87%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,6 +1,8 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
+        # create a hashmap to store element frequency
         counts = {}
         for i in arr:
             counts[i] = counts.get(i, 0) + 1
+        # check whether the number of unique elements and frequency is the same
         return len(counts) == len(set(counts.values()))
