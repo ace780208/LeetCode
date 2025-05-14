0402e5a8f80230fe25840dafe9d43989e239970f
Hsiao-Chien Shih <jxd270700@hotmail.com>
2025-05-14 16:40:36 -0700

    Time: 3 ms (13.46%), Space: 17.9 MB (36.73%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        # create a hasmap to count the frequency of letters from s with the given order
+        counts = {}
+        # create the output list
+        output = []
+        for ch in s:
+            if ch in order:
+                counts[ch] = counts.get(ch, 0) + 1
+            else:
+                output.append(ch)
+        
+        for ch in order:
+            output = output + [ch] * counts.get(ch, 0)
+        
+        return ''.join(output)
+            
