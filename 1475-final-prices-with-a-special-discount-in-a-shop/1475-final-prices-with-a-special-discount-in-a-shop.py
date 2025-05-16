ec590a7320dbebd9ca468630853792b0d81d0f89
Hsiao-Chien Shih <jxd270700@hotmail.com>
2025-05-16 16:01:18 -0700

    Time: 0 ms (100%), Space: 17.7 MB (95.48%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,14 +1,17 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
+        # this is the typical monotonic increase problem for recording price index
+        # create a stack to record price index
         stack = []
         ans = [0] * len(prices)
         for i in range(len(prices)):
+            # calculate discount price if the later index is first met
             while stack and prices[i] <= prices[stack[-1]]:
                 prev_idx = stack.pop()
                 ans[prev_idx] = prices[prev_idx] - prices[i]
 
             stack.append(i)
-        
+        # populate the rest price where discount does not apply
         while stack:
             idx = stack.pop()
             ans[idx] = prices[idx]
