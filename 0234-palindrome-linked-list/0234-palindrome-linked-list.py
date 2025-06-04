6834416dd615d7bcee7e7ab022cbd7262c6a0d45
Hsiao-Chien Shih <jxd270700@hotmail.com>
2025-06-04 13:49:44 -0700

    Time: 80 ms (6.71%), Space: 50.1 MB (5.23%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,41 +5,11 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # get the first and sec half of linked list through fast and slow pointers
-        sec = head
-        fast = head
-        prev = None
-        while fast and fast.next:
-            prev = sec
-            sec = sec.next
-            fast = fast.next.next
-
-        if prev:
-            prev.next = None
-        else:
-            return True
-
-        # reverse the linked list
-        reversed_nodes = self.reverse(sec)
-
-        # loop through both origin and the reverse linked list has the same val
-        dummy = head
-        while dummy:
-            first = dummy.val
-            second = reversed_nodes.val
-            if first != second:
-                return False
-            dummy = dummy.next
-            reversed_nodes = reversed_nodes.next
-
-        return True
-
-    def reverse(self, head: ListNode) -> ListNode:
-        prev = None
-        while head:
-            next_node = head.next
-            head.next = prev
-            prev = head
-            head = next_node
-        return prev
-    
+        self.left = ListNode(0, head)
+        def recursive(head):
+            if not head: 
+                return True
+            right = recursive(head.next)
+            self.left = self.left.next
+            return right and self.left.val == head.val
+        return recursive(head)
