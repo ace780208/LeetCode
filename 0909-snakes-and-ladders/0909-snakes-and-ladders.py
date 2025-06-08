73facd77805e8a2a0f9a7fe812191455c7db5f82
Hsiao-Chien Shih <jxd270700@hotmail.com>
2025-06-08 16:16:17 -0700

    Time: 91 ms (5.02%), Space: 18.5 MB (12.93%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -0,0 +1,65 @@
+from collections import deque
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        n = len(board)
+        end = n**2
+        odd = n%2 == 1
+        
+        def decode(i: int):
+
+            remainder = i%(2*n)
+            row = None
+            if remainder in (0, n):
+                row = n - i//n
+            else:    
+                row = (n-1) - i//n
+                
+            col = None
+            if not remainder:
+                col = 0
+            elif remainder > n:
+                col = 2*n - remainder
+            else:
+                col = remainder - 1
+            
+            return (row, col)
+        
+        queue = deque([(None, 1)])
+        seen = set()
+        seen.add((None, 1))
+        row, col = decode(1)
+        #print(row, col)
+        if board[row][col] != -1:
+            prev_start, prev_end = queue.popleft()
+            #print(prev_start, prev_end)
+            curr_end = board[row][col]
+            queue.append((prev_end, curr_end))
+            seen.add((prev_end, curr_end))
+            
+        roll = 0
+        
+        ladder = False
+        while queue:
+            square_cnt = len(queue)
+            roll += 1
+            for sq in range(square_cnt):
+                prev_start, prev_end = queue.popleft()
+                #print(prev_start, prev_end)
+                for dice_roll in range(1, 7):
+                    curr_end = prev_end + dice_roll
+                    if curr_end > end:
+                        break
+                    row, col = decode(curr_end)
+                    #print(curr_end, row, col)
+                    if board[row][col] != -1:
+                        curr_end = board[row][col]
+                    #print(prev_end, dice_roll)
+                    if curr_end == end:
+                        return roll
+                        
+                    if (prev_end, curr_end) not in seen:
+                        seen.add((prev_end, curr_end))
+                        queue.append((prev_end, curr_end))
+            
+        return -1
+                        
