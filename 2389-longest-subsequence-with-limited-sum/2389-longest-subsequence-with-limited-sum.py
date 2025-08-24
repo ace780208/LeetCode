9dfa9d9771094263cc44a13b83a1be9b782810ad
Hsiao-Chien Shih <jxd270700@hotmail.com>
2025-08-24 15:10:02 -0700

    Time: 7 ms (82.52%), Space: 18.2 MB (43.61%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,26 +1,25 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        # binary search on prefix sum of a soted nums will give the max length of nums for each query
         def binary_search(sortlist, target):
             left = 0
             right = len(sortlist)
             while left < right:
-                mid = (left + right) //2
+                mid = (left + right) // 2
                 if sortlist[mid] > target:
                     right = mid
                 else:
-                    left = mid+1
+                    left = mid + 1
 
             return left
 
-        
         nums.sort()
         prefix_sum = [0]
         for num in nums:
-            prefix_sum.append(num+prefix_sum[-1])
-        
-        #print(prefix_sum)
+            prefix_sum.append(num + prefix_sum[-1])
+
         ans = []
         for q in queries:
-            ans.append(binary_search(prefix_sum, q)-1)
-        
+            ans.append(binary_search(prefix_sum, q) - 1)
+
         return ans
